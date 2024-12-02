bad161c617cadf7354bf854c841454edc46a5b3c
Joao Vitor Cavalcanti Regis <jvcr@WINDOWS.CIN.UFPE.BR>
2024-12-02 15:23:52 -0300

    feat: calculator mvp implemented

--- app/src/main/java/regis/vitor/joao/applicationteste/MainActivity.kt
+++ app/src/main/java/regis/vitor/joao/applicationteste/MainActivity.kt
@@ -24,9 +24,44 @@ class MainActivity : ComponentActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.button.setOnClickListener() {
+        var value1: Double
+        var value2: Double
+        var result: Double
+
+        binding.sum.setOnClickListener() {
             binding.textView.text = "Botão acionado"
         }
+
+
+        binding.sum.setOnClickListener() {
+            value1 = binding.num1.text.toString().toDoubleOrNull() ?: 0.0
+            value2 = binding.num2.text.toString().toDoubleOrNull() ?: 0.0
+            result = value1 + value2
+            binding.resultView.text = String.format("%.2f", result)
+        }
+
+        binding.sub.setOnClickListener() {
+            value1 = binding.num1.text.toString().toDoubleOrNull() ?: 0.0
+            value2 = binding.num2.text.toString().toDoubleOrNull() ?: 0.0
+            result = value1 - value2
+            binding.resultView.text = String.format("%.2f", result)
+        }
+
+        binding.mul.setOnClickListener() {
+            value1 = binding.num1.text.toString().toDoubleOrNull() ?: 0.0
+            value2 = binding.num2.text.toString().toDoubleOrNull() ?: 0.0
+            result = value1 * value2
+            binding.resultView.text = String.format("%.2f", result)
+        }
+
+        binding.div.setOnClickListener() {
+            value1 = binding.num1.text.toString().toDoubleOrNull() ?: 0.0
+            value2 = binding.num2.text.toString().toDoubleOrNull() ?: 0.0
+            result = value1 / value2
+            binding.resultView.text = String.format("%.2f", result)
+        }
+
+
 //        setContentView(R.layout.activity_main)
 //
 //        val textView = findViewById<TextView>(R.id.textView)
